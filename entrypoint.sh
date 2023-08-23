67f8c15ea3912d78b07dfce1bca493eeec25486e
Hemen Showkati <h.showkati@gmail.com>
2023-08-23 17:26:40 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,14 @@ sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPA
 sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/nginx/nginx.conf
 
 
-# Create the V2Ray configuration URL
 config_url="vmess://ws+tls:$UUID-0@$DOPRAX_APP_URL:443/?path=$VMESS_WSPATH"
 
-# Echo out the V2Ray configuration URL
-echo "V2Ray Configuration URL: $config_url"
+
+echo "=========================================="
+echo "   Here is your config link"
+echo "=========================================="
+echo ""
+echo "Configuration URL:"
+echo "$config_url"
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
