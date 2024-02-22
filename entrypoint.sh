c9f792ff3d886575cd2ec5f03e344ce983cc52ad
Hemen Showkati <h.showkati@gmail.com>
2024-02-22 23:13:32 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,46 @@
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/v2ray/config.json
 sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/nginx/nginx.conf
 
-
-
-
-
-echo "=========================================="
-echo "   Here is your V2ray config link"
-echo ""
-echo "vmess://ws+tls:$UUID-0@$DOPRAX_APP_URL:443/?path=$VMESS_WSPATH"
-echo ""
-echo "=========================================="
-
+# Echo the JSON configuration
+echo "{"
+echo "    \"outbounds\": ["
+echo "        {"
+echo "            \"mux\": {"
+echo "            },"
+echo "            \"protocol\": \"vmess\","
+echo "            \"sendThrough\": \"0.0.0.0\","
+echo "            \"settings\": {"
+echo "                \"vnext\": ["
+echo "                    {"
+echo "                        \"address\": \"$DOPRAX_APP_URL\","
+echo "                        \"port\": 443,"
+echo "                        \"users\": ["
+echo "                            {"
+echo "                                \"id\": \"$UUID\""
+echo "                            }"
+echo "                        ]"
+echo "                    }"
+echo "                ]"
+echo "            },"
+echo "            \"streamSettings\": {"
+echo "                \"network\": \"ws\","
+echo "                \"security\": \"tls\","
+echo "                \"tlsSettings\": {"
+echo "                    \"disableSystemRoot\": false"
+echo "                },"
+echo "                \"wsSettings\": {"
+echo "                    \"headers\": {"
+echo "                        \"Host\": \"\""
+echo "                    },"
+echo "                    \"path\": \"$VMESS_WSPATH\""
+echo "                },"
+echo "                \"xtlsSettings\": {"
+echo "                    \"disableSystemRoot\": false"
+echo "                }"
+echo "            },"
+echo "            \"tag\": \"PROXY\""
+echo "        }"
+echo "    ]"
+echo "}"
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
